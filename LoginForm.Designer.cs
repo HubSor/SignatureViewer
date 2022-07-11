2c3bdff54220f8b7a871c04790154b8491787e04
HubSor <hubisoroka@gmail.com>
2022-07-11 11:48:48 +0200

    Initial commit. Simple form & viewing done.

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SignatureRetriever
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.ResultBrowser = new System.Windows.Forms.WebBrowser();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoginBox
+            // 
+            this.LoginBox.Location = new System.Drawing.Point(65, 36);
+            this.LoginBox.Name = "LoginBox";
+            this.LoginBox.Size = new System.Drawing.Size(276, 20);
+            this.LoginBox.TabIndex = 1;
+            this.LoginBox.TextChanged += new System.EventHandler(this.LoginBox_TextChanged);
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(65, 62);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(276, 20);
+            this.PasswordBox.TabIndex = 2;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(6, 65);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.AutoSize = true;
+            this.LoginLabel.Location = new System.Drawing.Point(6, 39);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(26, 13);
+            this.LoginLabel.TabIndex = 4;
+            this.LoginLabel.Text = "Mail";
+            this.LoginLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ResultBrowser
+            // 
+            this.ResultBrowser.Location = new System.Drawing.Point(18, 117);
+            this.ResultBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.ResultBrowser.Name = "ResultBrowser";
+            this.ResultBrowser.Size = new System.Drawing.Size(323, 440);
+            this.ResultBrowser.TabIndex = 5;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ConnectButton.Location = new System.Drawing.Point(139, 88);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(80, 23);
+            this.ConnectButton.TabIndex = 6;
+            this.ConnectButton.Text = "Log in";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(329, 24);
+            this.TitleLabel.TabIndex = 7;
+            this.TitleLabel.Text = "Pass credentials to get email signature";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 585);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.ResultBrowser);
+            this.Controls.Add(this.LoginLabel);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.LoginBox);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LoginBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Label LoginLabel;
+        private System.Windows.Forms.WebBrowser ResultBrowser;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Label TitleLabel;
+    }
+}
+
---
+++ LoginForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace SignatureRetriever
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+            Uri redirectionUri = new Uri(redirectionUrl);
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            var mail = LoginBox.Text;
+            var passwd = PasswordBox.Text;
+
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
+            service.Credentials = new WebCredentials(mail, passwd);
+            service.AutodiscoverUrl(mail, RedirectionUrlValidationCallback);
+            var owaConfig = UserConfiguration.Bind(service, "OWA.UserOptions", WellKnownFolderName.Root, UserConfigurationProperties.All);
+            if (owaConfig.Dictionary.ContainsKey("signaturehtml"))
+            {
+                var signature = owaConfig.Dictionary["signaturehtml"];
+                ResultBrowser.DocumentText = signature.ToString();
+            }
+            else ResultBrowser.DocumentText = "No niestety";
+        }
+    }
+}
